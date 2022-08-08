41c413344771b20eb683486de2feb6b36854554d
wiz <wilsonzhou92@gmail.com>
2022-08-08 18:14:26 +0200

    crud update

--- src/etl/crud/__init__.py
+++ src/etl/crud/__init__.py
@@ -12,14 +12,18 @@ class CRUD:
     def create(self, rows: Iterable[SQLModel], batch_size: int=1_000) -> None:
         assert all(isinstance(row, self.table) for row in rows)
 
-        for partition in tqdm(list(partition_all(batch_size, rows))):
-            with Session(self.engine) as session:
-                try:
-                    for row in partition:
-                        session.add(row)
-                    session.commit()
-                except:
-                    session.rollback()
+        session = Session(self.engine)
+        try:
+            for partition in tqdm(list(partition_all(batch_size, rows))):
+                for row in partition:
+                    session.add(row)
+                session.commit()
+        except Exception as e:
+            print(e)
+            session.rollback()
+        finally:
+            session.close()
+
 
     def read(self, filters: list=None) -> list[SQLModel]:
         with Session(self.engine) as session:
@@ -35,13 +39,15 @@ class CRUD:
     def delete(self, filters: list=None, batch_size: int=1_000) -> None:
         result = self.read(filters)
 
-        for partition in tqdm(list(partition_all(batch_size, result))):
-            with Session(self.engine) as session:
-                try:
-                    for row in partition:
-                        session.delete(row)
-                    session.commit()
-                except Exception as e:
-                    print(e)
-                    session.rollback()
+        session = Session(self.engine)
+        try:
+            for partition in tqdm(list(partition_all(batch_size, result))):
+                for row in partition:
+                    session.delete(row)
+                session.commit()
+        except Exception as e:
+            print(e)
+            session.rollback()
+        finally:
+            session.close()
 
